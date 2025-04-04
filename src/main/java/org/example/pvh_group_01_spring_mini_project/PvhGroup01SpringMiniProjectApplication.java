1852c238352575ca59dccb87f6d510d1e0d6cd64
MonyVisaSU4 <visagoodboy042@gmail.com>
2025-04-04 14:26:12 +0700

    Mini-Project

---
+++ src/main/java/org/example/pvh_group_01_spring_mini_project/PvhGroup01SpringMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.pvh_group_01_spring_mini_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvhGroup01SpringMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PvhGroup01SpringMiniProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pvh_group_01_spring_mini_project/PvhGroup01SpringMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pvh_group_01_spring_mini_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvhGroup01SpringMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
