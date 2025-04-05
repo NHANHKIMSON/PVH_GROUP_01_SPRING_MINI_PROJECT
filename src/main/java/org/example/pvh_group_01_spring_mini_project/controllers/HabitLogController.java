6d956729d05847063a9a2278e6fb4664ceafbb48
Leang Chhengleap <chhing020518@gmail.com>
2025-04-05 23:50:01 +0700

    Test commit

--- src/main/java/org/example/pvh_group_01_spring_mini_project/controllers/HabitLogController.java
+++ src/main/java/org/example/pvh_group_01_spring_mini_project/controllers/HabitLogController.java
@@ -1,9 +1,21 @@
 package org.example.pvh_group_01_spring_mini_project.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+
+//This is after I have clone and write code from here
+import org.example.pvh_group_01_spring_mini_project.models.entity.HabitLog;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/habit-logs")
 public class HabitLogController {
+    @GetMapping
+    public HabitLog getHabitLog() {
+        return null;
+    }
+
+    @PostMapping
+    public HabitLog addHabitLog(@RequestBody HabitLog habitLog) {
+        return null;
+    }
 }
