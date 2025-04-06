b6fe095f5703c051142b91889d0ba9c468e016b8
Veasna <veasnasom@example.com>
2025-04-06 11:59:35 +0700

    dcode password

--- src/main/java/org/example/pvh_group_01_spring_mini_project/service/impl/UserAppServiceImple.java
+++ src/main/java/org/example/pvh_group_01_spring_mini_project/service/impl/UserAppServiceImple.java
@@ -7,14 +7,20 @@ import org.example.pvh_group_01_spring_mini_project.repository.UserAppRepository
 import org.example.pvh_group_01_spring_mini_project.service.UserAppService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.beans.Encoder;
 
 @Service
 @RequiredArgsConstructor
 public class UserAppServiceImple implements UserAppService {
     private final UserAppRepository userAppRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public UserApp registerProfile(AuthRegisterRequest authRegisterRequest) {
+        authRegisterRequest.setPassword(passwordEncoder.encode(authRegisterRequest.getPassword()));
         return userAppRepository.registerProfile(authRegisterRequest);
     }
 
