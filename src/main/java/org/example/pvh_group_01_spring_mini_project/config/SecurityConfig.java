de1144bc214bdb351191185f4271a9e49b19dc10
MonyVisaSU4 <visagoodboy042@gmail.com>
2025-04-06 13:20:22 +0700

    Your commit message describing your changes

--- src/main/java/org/example/pvh_group_01_spring_mini_project/config/SecurityConfig.java
+++ src/main/java/org/example/pvh_group_01_spring_mini_project/config/SecurityConfig.java
@@ -1,18 +1,8 @@
-//package org.example.pvh_group_01_spring_mini_project.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//public class SecurityConfig {
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package org.example.pvh_group_01_spring_mini_project.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+}
